7e9375edd3accfa0ccfacf5419e7387b6caa39bf
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2023-11-02 08:51:56 +0800

    added sendNotification.php

---
+++ api/sendNotification.php
@@ -0,0 +1,21 @@
+<?php      
+  require_once("./vendor/autoload.php");
+
+  use Minishlink\WebPush\Subscription;
+  use Minishlink\WebPush\WebPush;
+  
+  class Notification{
+    function sendNotif($token){
+      $auth = [
+        'VAPID' => [
+            'subject' => 'mailto:me@website.com', // can be a mailto: or your website address
+            'publicKey' => 'BKpp3bZGmXDPhvW4Zxf9CBybvQ6oH4gKOEfybeid60ncfQ61E7LQxs70sNOyX9sXcS5C-03nju19QwlYq5vsSQQ', // (recommended) uncompressed public key P-256 encoded in Base64-URL
+            'privateKey' => 'v6KkmZpU0EAd8_cEAZCW_klkQ8HA3qr5iDC2rFXIgM0'
+        ],
+      ];
+    
+      $webPush = new WebPush($auth);
+    }
+  }
+
+
