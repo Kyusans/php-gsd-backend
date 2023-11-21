077aea3f029c898da601a93b2d545b4455b25854
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2023-11-21 11:28:41 +0800

    fixed sendNotification.php, error: Subscription::create(NULL, true)\n#1

--- api/sendNotification.php
+++ api/sendNotification.php
@@ -1,23 +1,33 @@
-<?php      
-  require_once("./vendor/autoload.php");
+<?php
+require_once("./vendor/autoload.php");
 
-  use Minishlink\WebPush\Subscription;
-  use Minishlink\WebPush\WebPush;
-  
-  class Notification{
-    function sendNotif($token, $subject, $message){
-      $auth = [
-        'VAPID' => [
-            'subject' => 'mailto:me@website.com',
-            'publicKey' => 'BKpp3bZGmXDPhvW4Zxf9CBybvQ6oH4gKOEfybeid60ncfQ61E7LQxs70sNOyX9sXcS5C-03nju19QwlYq5vsSQQ',
-            'privateKey' => 'v6KkmZpU0EAd8_cEAZCW_klkQ8HA3qr5iDC2rFXIgM0'
-        ],
-      ];
-      $webPush = new WebPush($auth);
-      $url = 'https://coc-studentinfo.net/gsd/';
-      $payload = ['title' => $message, 'body' => $subject, 'url' => $url];
-      $tokenArray = json_decode($token, true);
-      $subscription = Subscription::create($tokenArray, true);
-      $webPush->sendOneNotification($subscription, json_encode($payload), ['TTL' => 5000]);
+use Minishlink\WebPush\Subscription;
+use Minishlink\WebPush\WebPush;
+
+class Notification
+{
+    function sendNotif($token, $subject, $message)
+    {
+        $auth = [
+            'VAPID' => [
+                'subject' => 'mailto:me@website.com',
+                'publicKey' => 'BKpp3bZGmXDPhvW4Zxf9CBybvQ6oH4gKOEfybeid60ncfQ61E7LQxs70sNOyX9sXcS5C-03nju19QwlYq5vsSQQ',
+                'privateKey' => 'v6KkmZpU0EAd8_cEAZCW_klkQ8HA3qr5iDC2rFXIgM0'
+            ],
+        ];
+        $webPush = new WebPush($auth);
+        $url = 'https://coc-studentinfo.net/gsd/';
+        $payload = ['title' => $message, 'body' => $subject, 'url' => $url];
+        $tokenArray = json_decode($token, true);
+
+        try {
+            if ($tokenArray !== null) {
+                $subscription = Subscription::create($tokenArray, true);
+                $webPush->sendOneNotification($subscription, json_encode($payload), ['TTL' => 5000]);
+            }
+        } catch (TypeError $e) {
+            error_log('Caught TypeError: ' . $e->getMessage());
+        }
     }
-  }
+}
+?>
